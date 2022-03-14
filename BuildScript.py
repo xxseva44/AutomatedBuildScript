7d60b3226c91b8930e6b7e3571754abc90a6a900
G <77701714+xxseva44@users.noreply.github.com>
2022-03-14 19:27:30 -0400

    Update BuildScript.py

--- BuildScript.py
+++ BuildScript.py
@@ -3,9 +3,16 @@ import os
 
 def TextBreak ():
 	print("#############################")
+	print("#############################")
 
 def Rom_Check ():
-	Rom = input("What rom are you building")
+	global Rom
+	Rom = input("What rom are you building \n")
+
+
+def Thread_Check():
+	global Thread
+	Thread = input("How many threads does your cpu have?")
 
 
 def Working_DirCheck ():
@@ -14,7 +21,6 @@ def Working_DirCheck ():
 	if DirCon == "Y":
 		print("Directory set")
 	elif DirCon == "N":
-		TextBreak()
 		DirSelect()
 	else:
 		print("Please choose a valid option!!!")
@@ -22,59 +28,60 @@ def Working_DirCheck ():
 
 
 def DirSelect ():
+	os.chdir("/home")
+	subprocess.call(['pwd'])
 	Dir = input("Please enter the working directory \n")
-	TextBreak()
 	os.chdir(Dir)
 	print (f"{Dir} is now selected!")
 
 
 
 def Android_VersionCheck ():
+	global AndroidVersion
 	AndroidVersion = int(input("What android version is being built? Ex. 10,11,12 \n"))
 	if AndroidVersion > 11:
-		os.system('. build/envsetup.sh')
+		os.system(". build/envsetup.sh")
 	elif AndroidVersion <= 11:
-		os.system('source build/envsetup.sh')
+		os.system("source build/envsetup.sh")
 	else:
 		print ("Please choose a valid option!")
-		TextBreak()
 		Android_VersionCheck()
-	fi
 
 
 def Ccache_Config ():
-	CcacheCon = input("Enable ccache? Y/N")
+	CcacheCon = input("Enable ccache? Y/N \n")
 	if CcacheCon == "Y":
-		subprocess.call(['export USE_CCACHE=1'])
-		CcacheSpace = input("How much space would you like too allocate in gigabytes? Ex. 50")
-		subprocess.call(['ccache -M {CcacheSpace}G'])
+		os.system("export USE_CCACHE=1")
+		CcacheSpace = input("How much space would you like too allocate in gigabytes? Ex. 50G \n")
+		os.system("ccache -M" + CcacheSpace)
 	elif CcacheCon == "N":
-		subprocess.call(['export USE_CCACHE=0'])
-		fi
+		subprocess.run('export USE_CCACHE=0')
 
 
 def DeviceVariant_BuildConfig ():
+	global DeviceVar
 	DeviceVar = input("What device/variant would you like to build? \n")
 	print(f"Building android {AndroidVersion} for {DeviceVar}")
 	if AndroidVersion <= 11:
-		subprocess.call(['lunch {Rom}_{DeviceVar}'])
+		os.system("lunch " + Rom + "_" + DeviceVar)
 	elif AndroidVersion > 12:
-		subprocess.call(['lunch {Rom}_{DeviceVar}-{BuildType}'])
+		os.system("lunch " + Rom + "_" + DeviceVar + "-" + BuildType)
 	print(f"lunch {Rom}_{DeviceVar}")
 
 
 def Build_Type ():
-	print("What build would you like to make? \n")
-	BuildType = input("Options: user|userdebug|eng")
+	print("What build would you like to make?")
+	global BuildType
+	BuildType = input("Options: user|userdebug|eng \n")
 
 
 def Start_Build ():
 	print("Starting Build")
 	if AndroidVersion <= 11:
-		 subprocess.call(['bsubprocess.callch {Rom}_{DeviceVar}-{BuildType}'])
-		 print(f"{Rom}_{DeviceVar}-{BuildType}")
+		os.system("brunch " + Rom + "_" + DeviceVar + "-" + BuildType)
+		print(f"{Rom}_{DeviceVar}-{BuildType}")
 	elif AndroidVersion > 12:
-		subprocess.call(['make $rom -j$nproc'])
+		os.system("make " + Rom + "-j" + Thread)
 		print("Starting build, please wait...")
 
 
@@ -82,10 +89,19 @@ print("########################")
 print("#Automated Build Script#")
 print("#By:   XXSEVA44@XDA    #")
 print("########################")
+TextBreak()
 Working_DirCheck()
+TextBreak()
 Rom_Check()
+TextBreak()
 Android_VersionCheck()
+TextBreak()
 Ccache_Config()
+TextBreak()
 Build_Type()
+TextBreak()
 DeviceVariant_BuildConfig()
+TextBreak()
 Start_Build()
+TextBreak()
+TextBreak()
